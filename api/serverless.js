5015e4a7a697c6047b45b712bcbfbf168d25b6be
RostiMelk <hello@rosti.no>
2025-01-31 14:37:48 +0100

    testing

--- api/serverless.js
+++ api/serverless.js
@@ -1,13 +1,22 @@
-// Read the .env file.
 import * as dotenv from "dotenv";
 dotenv.config();
 
-// Require the framework
 import { init } from "../public/server.js";
 
-export default async (req, res) => {
-  console.log("Request received", req.url);
+export default async function handler(req, res) {
   const app = init();
   await app.ready();
-  app.server.emit("request", req, res);
-};
+
+  // Log the incoming request for debugging
+  console.log("Incoming request:", {
+    method: req.method,
+    url: req.url,
+    path: req.path,
+  });
+
+  return new Promise((resolve, reject) => {
+    app.server.emit("request", req, res);
+    res.on("finish", resolve);
+    res.on("error", reject);
+  });
+}
