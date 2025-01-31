9b5ca0e16ddfd6995802739f278a26e190886820
RostiMelk <hello@rosti.no>
2025-01-31 15:41:56 +0100

    feat: test

--- src/app.ts
+++ src/app.ts
@@ -3,7 +3,7 @@ import { createImage } from "./core/createImage.js";
 import { Snippet } from "./types/index.js";
 
 const root: FastifyPluginAsync = async (fastify, opts): Promise<void> => {
-  fastify.get("/", async (request, reply) => {
+  fastify.post("/api/image", async (request, reply) => {
     const body = request.body;
     const parsedBody = Snippet.parse(body);
     const imageResponse = await createImage(parsedBody);
